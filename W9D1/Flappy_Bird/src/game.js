5b309af2f1028274ad57ada5da797ed6b55fa522
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-08-31 17:34:14 -0400

    Started Flappy Bird kinda

--- W9D1/Flappy_Bird/src/game.js
+++ W9D1/Flappy_Bird/src/game.js
@@ -1,6 +1,19 @@
+
+import Level from "./level"
+
 export default class FlappyBird {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+
+    
+  }
+  // drawing the background
+  animate() {
+    this.level.animate(this.ctx)
+  }
+
+  restart() {
+    
   }
 }
